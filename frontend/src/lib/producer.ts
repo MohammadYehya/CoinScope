3605a7ef75ef1953fc099629205284e79f37079b
MohammadYehya <mohammad7446@gmail.com>
2025-02-15 01:38:02 +0500

    Create Kafka Producer

---
+++ frontend/src/lib/producer.ts
@@ -0,0 +1,17 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "nextjs-app",
+  brokers: ["kafka:9092"], // Update this if using a cloud service
+});
+
+const producer = kafka.producer();
+
+export const sendMessage = async (topic: string, message: string) => {
+  await producer.connect();
+  await producer.send({
+    topic,
+    messages: [{ value: message }],
+  });
+  await producer.disconnect();
+};
