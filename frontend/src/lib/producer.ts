c279bde9a1be1e1cbd09ae476c1424bc09b521b5
MohammadYehya <mohammad7446@gmail.com>
2025-02-16 01:56:23 +0500

    Update producer.ts

--- frontend/src/lib/producer.ts
+++ frontend/src/lib/producer.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "nextjs-app",
-  brokers: ["kafka:9092"], // Update this if using a cloud service
+  brokers: [(process.env.KAFKAPATH || "localhost")+":9092"], // Update this if using a cloud service
 });
 
 const producer = kafka.producer();
