98b0fbc2cbb12d0cdc5e22f28a323d28b8322bc4
MohammadYehya <mohammad7446@gmail.com>
2025-02-16 01:55:23 +0500

    Delete route.ts

--- frontend/src/app/api/Consume/route.ts
+++
@@ -1,7 +0,0 @@
-import { startConsumer } from "@/lib/consumer";
-
-export async function GET() {
-  startConsumer("test-topic", (message) => {
-    console.log("Received message:", message);
-  });
-}
